710ca2507f528b31952460e3e6dc81230b92033a
Kyrus2504 <anony100263@gmail.com>
2023-05-09 16:23:33 +1000

    Index page shaping up to be a certified banger

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hertz UTS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar">
+        <a class="logo-anchor">
+            <img class="logo-img" src="" alt="Hertz UTS Logo"></img>
+        </a>
 
+        <button class="shopping-cart">
+            <img class="shopping-cart-img" src="" alt="Shopping Cart"></img>
+        </button>
+    </nav>
+    <div class="card" style="width: 33rem; height: 20rem; margin-left: 10%; margin-top: 3%;">
+    <img class="card-img-top" src="" alt="Example Car Image"></img>
+    <div class="card-body">
+    <h5 class="card-title">Car Name</h5>
+    <p class="card-text">This Nissan Pigeon is the leading quality avian transportation device</p>
+    <a href="#" class="btn btn-primary">Add to cart</a>
+  </div>
+</div> 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
