8c4b408ea2333d5fa9c8fb444596c932102e8235
Kyrus2504 <anony100263@gmail.com>
2023-06-02 15:18:50 +1000

    getting cart functionality to work

--- index.php
+++ index.php
@@ -1,60 +1,72 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hertz UTS</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link href="style.css" rel="stylesheet">
-  </head>
-  <body>
+
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Hertz UTS</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link href="style.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
   <script type="text/javascript" src="cars.json"></script>
-  <script defer src="script.js"></script>
-    <nav class="navbar">
-        <a class="logo-anchor" href="index.php">
-            <img class="logo-img" src="" alt="Hertz UTS Logo"></img>
-        </a>
+  <script defer src="script.js">
+  </script>
+</head>
 
-        <button class="shopping-cart" data-modal-target="#modal">
-            <img class="shopping-cart-img" src="" alt="Shopping Cart"></img>
-        </button>
-    </nav>
-    <div class="row row-cols-1 row-cols-md-2">
+
+<body onload="indexReady()">
+  <nav class="navbar">
+    <a class="logo-anchor" href="index.php">
+      <img class="logo-img" src="imgs/Hertz UTS.png" alt="Hertz UTS Logo"></img>
+    </a>
+
+    <button class="shopping-cart btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      <img class="shopping-cart-img" src="imgs/shoppingCart.jpg" alt="Shopping Cart"></img>
+    </button>
+
+
+  </nav>
+
+
+  <div class="row row-cols-1 row-cols-md-2">
     <div class="card-instance">
-</div>
-</div>
-
-
-<div class="modal" id="modal">
-    <div class="modal-header">
-      <div class="title">Shopping Cart</div>
-      <button data-close-button class="close-button">&times;</button>
     </div>
-    <div class="cart-body">
-    <div class="cart-instance">
-                <span class="cart-item cart-header cart-column">ITEM</span>
-                <span class="cart-price cart-header cart-column">PRICE</span>
-                <span class="cart-quantity cart-header cart-column">QUANTITY</span>
-            </div>
-    </div>
-    <div class="cart-footer">
-      <div class ="cart-item-list">
-        
-      </div>
-    <div class="cart-total">
-                <strong class="cart-total-title">Total</strong>
-                <span class="cart-total-price">$0</span>
-            </div>
-  <button class="clear-cart-button" type="button">Clear Cart</button>
-  <a class="checkout-button" href='checkout.php'>
-    <button >Checkout</button>
-    <a>
-</div>
   </div>
-  
-  <div id="overlay"></div>
-  </body>
+
+  </div>
+
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="cart-body">
+            <div class="cart-instance">
+              <span class="cart-item cart-header cart-column">Car</span>
+              <span class="cart-price cart-header cart-column">Price</span>
+              <span class="cart-quantity cart-header cart-column">Rent Days</span>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <div class="cart-total">
+            <strong class="cart-total-title">Total</strong>
+            <span class="cart-total-price">$0</span>
+          </div>
+          <button class="clear-cart-button" type="button">Clear Cart</button>
+          <a class="checkout-button" href='checkout.php'>
+            <button>Checkout</button>
+            <a>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
 </html>
--- script.js
+++ script.js
@@ -22,54 +22,195 @@ async function getData(file, callback) {
 
 //usage:
 
-getData("cars.json", function (text) {
-  var data = JSON.parse(text);
-  console.log(data);
-  for (x = 0; x < data.length; x++) {
-    var cardRow = document.createElement("div");
-    var cardItems = document.getElementsByClassName("card-instance")[0];
-    var cardRowContents = `
+function indexReady() {
+  getData("cars.json", function (text) {
+    var data = JSON.parse(text);
+    console.log(data);
+    for (x = 0; x < data.length; x++) {
+      var cardRow = document.createElement("div");
+      var cardItems = document.getElementsByClassName("card-instance")[0];
+      var cardRowContents = `
     
     <div class="card card-instance col mb-4" style="width: 33rem; height: 20rem; margin-left: 10%; margin-top: 3%;">
-    <img class="card-img-top" src="" alt="Example Car Image"></img>
+    <img class="card-img-top" src="${data[x].Image}" alt="${data[x].Name} Car Image" style="width: 160px; height: 90px; margin-left: 33%;"></img>
     <div class="card-body">
     <h5 class="card-title">${data[x].Name}</h5>
     <p class="card-description">${data[x].Description}</p>
     <p class="card-price" style="display: inline; margin-right: 25%; margin-top: 10%;">$${data[x].Price}</p>
-    <a href="#" class="btn btn-primary" style="">Add to cart</a>
+    <input type="submit" value="Add to Cart" class="add-item-button"></input>
   </div>
 </div>
 `;
-    cardRow.innerHTML = cardRowContents;
-    cardItems.append(cardRow);
-  }
-});
+      cardRow.innerHTML = cardRowContents;
+      cardItems.append(cardRow);
+    }
+  });
+}
 
-function ready() {}
+//Shopping Cart & Add to Cart functions
+
+function ready() {
+  var removeItemButton = document.getElementsByClassName("btn-danger");
+  for (var i = 0; i < removeItemButton.length; i++) {
+    var button = removeItemButton[i];
+    button.addEventListener("click", removeCartItem);
+  }
+
+  var removeCartButton =
+    document.getElementsByClassName("clear-cart-button")[0];
+  removeCartButton.addEventListener("click", removeCartContent);
+
+  var addToCartButtons = document.getElementsByClassName("add-item-button");
+  for (var i = 0; i < addToCartButtons.length; i++) {
+    var button = addToCartButtons[i];
+    button.addEventListener("click", addToCartClicked);
+  }
+  console.log(addToCartButtons);
+
+  var changeQuantity = document.getElementsByClassName("cart-quantity-input");
+  for (var i = 0; i < changeQuantity.length; i++) {
+    var quantityInput = changeQuantity[i];
+    quantityInput.addEventListener("change", changeQuantities);
+  }
+
+  console.log("Ready complete");
+  //checkout button, couldnt get to work
+
+  /*var addToCartButtons = document.getElementsByClassName("submit-order");
+  for (var i = 0; i < addToCartButtons.length; i++) {
+    var button = addToCartButtons[i];
+    button.addEventListener("click", submitOrder);
+  }*/
+  //cart
+}
+
+function removeCartItem(event) {
+  var removeButton = event.target;
+  removeButton.parentElement.parentElement.remove();
+  console.log("pre remove button update cart reached");
+  updateCartTotal();
+}
+
+function removeCartContent() {
+  var cartBody = document.getElementsByClassName("cart-body")[0];
+  cartBody.innerHTML = `<div class="cart-instance">
+  <span class="cart-item cart-header cart-column">Car</span>
+  <span class="cart-price cart-header cart-column">Price</span>
+  <span class="cart-quantity cart-header cart-column">Rent Days</span>
+</div>
+</div>`;
+  updateCartTotal();
+}
+
+function addToCartClicked(event) {
+  console.log("Add to cart clicked");
+  var button = event.target;
+  var Item = button.parentElement.parentElement;
+  var title = Item.getElementsByClassName("card-title")[0].innerText;
+  console.log(title);
+  var price = Item.getElementsByClassName("card-price")[0].innerText;
+  console.log(price);
+  var image = Item.getElementsByClassName("card-img-top")[0].src;
+  console.log(image);
+  quantity = 1;
+  addItemToCart(title, price, image, quantity);
+  console.log("Reached pre update cart");
+  updateCartTotal();
+}
+
+function changeQuantities(event) {
+  console.log("Change Quantities reached");
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateCartTotal();
+}
+
+function addItemToCart(title, price, image, quantity) {
+  var cartRow = document.createElement("div");
+  var cartItems = document.getElementsByClassName("cart-body")[0];
+  //var cartItemRows = document.getElementsByClassName("cart-row");
+  var cartItemNames = cartItems.getElementsByClassName("cart-item-title");
+  for (var i = 0; i < cartItemNames.length; i++) {
+    if (cartItemNames[i].innerText == title) {
+      var cartQuantityUpdate = cartItems[i];
+      cartQuantityUpdate.querySelector(".cart-quantity-input").value += 1;
+      console.log(
+        cartQuantityUpdate.querySelector(".cart-quantity-input").value
+      );
+      return;
+    }
+  }
+  var cartRowContents = `
+  <div class="cart-row">
+    <div class="cart-item cart-column">
+      <img class="cart-item-image" src="${image}" width="50" height="50">
+      <span class="cart-item-title">${title}</span>
+    </div>
+    <span class="cart-price cart-price-instance cart-column">${price}</span>
+    <div class="cart-quantity cart-column">
+      <input class="cart-quantity-input" type="number" value="${quantity}">
+      <button style="display: flex;" class="btn btn-danger btn-instance" type="button">REMOVE</button>
+    </div>
+  </div>
+`;
+  cartRow.innerHTML = cartRowContents;
+  cartItems.append(cartRow);
+  cartRow
+    .getElementsByClassName("btn-danger")[0]
+    .addEventListener("click", removeCartItem);
+}
+
+function updateCartTotal() {
+  console.log("reached update cart");
+  var cartItemContainer = document.getElementsByClassName("cart-body")[0];
+  console.log(cartItemContainer);
+  var cartRows = cartItemContainer.getElementsByClassName("cart-row");
+  console.log(cartRows);
+  var total = 0;
+  for (var x = 0; x < cartRows.length; x++) {
+    var cartRow = cartRows[x];
+    var price = cartRow.getElementsByClassName("cart-price")[0];
+    console.log(price);
+    var quantity = cartRow.getElementsByClassName("cart-quantity-input")[0];
+    console.log(quantity);
+    var actualPrice = parseFloat(price.innerText.replace("$", ""));
+    var actualQuantity = quantity.value;
+    total = total + actualPrice * actualQuantity;
+  }
+  total = Math.round(total * 100) / 100;
+  console.log(total);
+  document.getElementsByClassName("cart-total-price")[0].innerText =
+    "$" + total;
+}
 
 //shopping cart
 
-const openModalButtons = document.querySelectorAll("[data-modal-target]");
+/*const openModalButtons = document.querySelectorAll("[data-modal-target]");
 const closeModalButtons = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
 
 openModalButtons.forEach((button) => {
   button.addEventListener("click", () => {
+    console.log("open modal reached");
     const modal = document.querySelector(button.dataset.modalTarget);
+    console.log(modal);
     openModal(modal);
   });
 });
 
 overlay.addEventListener("click", () => {
-  const modals = document.querySelectorAll(".modal.active");
+  const modals = document.querySelectorAll(".modalCart.active");
   modals.forEach((modal) => {
+    console.log("close modal reached");
     closeModal(modal);
   });
 });
 
 closeModalButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    const modal = button.closest(".modal");
+    const modal = button.closest(".modalCart");
     closeModal(modal);
   });
 });
@@ -84,4 +225,4 @@ function closeModal(modal) {
   if (modal == null) return;
   modal.classList.remove("active");
   overlay.classList.remove("active");
-}
+}*/
